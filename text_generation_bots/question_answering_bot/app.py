9d025bc46fa1210cf6bbc095337cb676b0206d93
MasemeneMatlakanaBenny <bennymasemene46@gmail.com>
2025-11-24 15:18:30 +0200

    added qa_answer pipeline

--- text_generation_bots/question_answering_bot/app.py
+++ text_generation_bots/question_answering_bot/app.py
@@ -1,1 +1,7 @@
+import torch
+import gradio as gr
+from transformers import pipeline
+
+qa_answer=qa_answer= pipeline("question-answering", model="distilbert/distilbert-base-uncased-distilled-squad")
+
 
