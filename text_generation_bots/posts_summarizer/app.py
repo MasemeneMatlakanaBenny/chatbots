400e19864adf1264229d32813b41a70f9f50ffc0
MasemeneMatlakanaBenny <bennymasemene46@gmail.com>
2025-11-24 05:53:01 +0200

    added news summarized

--- text_generation_bots/posts_summarizer/app.py
+++ text_generation_bots/posts_summarizer/app.py
@@ -3,3 +3,10 @@ import torch
 from transformers import pipeline
 from web_scrapping import extract_article_text
 
+def news_summarizer(link,history):
+  summarizer = pipeline("summarization", model="Falconsai/text_summarization")
+  article_text=extract_article_text(url=link)
+
+  summary=summarizer(article_text,min_length=50,max_length=1000)
+
+  return summary[0]['summary_text']
