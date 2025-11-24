7496a725ede6fd1383e0c27de5867f4a052ba6ef
MasemeneMatlakanaBenny <bennymasemene46@gmail.com>
2025-11-24 15:19:07 +0200

    added qa chatbot

--- text_generation_bots/question_answering_bot/app.py
+++ text_generation_bots/question_answering_bot/app.py
@@ -5,3 +5,17 @@ from transformers import pipeline
 qa_answer=qa_answer= pipeline("question-answering", model="distilbert/distilbert-base-uncased-distilled-squad")
 
 
+def qa_chatbot(question,history):
+  context=article
+
+  results=qa_answer(question=question,context=context)
+
+  return results['answer']
+
+
+chatbot=gr.ChatInterface(
+    fn=qa_chatbot,
+    title="Question-Answering Chatbot",
+    description="Enter the link of any article or posts and get some answers"
+)
+
